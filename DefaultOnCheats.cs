d3e5b778fd13b580c709800b3a5ecedc87f17923
EnderTedi <endertedi2@gmail.com>
2024-03-25 19:09:37 +0100

    DefaultOnCheats

---
+++ DefaultOnCheats.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultOnCheats
+{
+    public class DefaultOnCheats : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Program.enableCheats = true;
+        }
+    }
+}
